4d5f40f4423345aeba6c13773e7446de4b92f54a
Nour-Eldeen98 <nour.nasser@bduro.com>
2024-07-13 19:46:48 +0300

    start

---
+++ src/components/mainLayout/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row ">
+      {/* Logo */}
+      {/* Menu in large screens */}
+      {/* Mobile Menu */}
+    </div>
+  );
+};
+
+export default Header;
